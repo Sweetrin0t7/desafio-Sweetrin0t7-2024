be5301d4ab1cd29a1dad44df4365caab0922c21d
Sweetrin0t7 <renataunisj@gmail.com>
2024-09-09 23:32:49 -0300

    Retirando comentários e finalizando teste

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -19,15 +19,12 @@ class RecintosZoo {
   }
 
 
-  //1.2 - O programa deve receber tipo e quantidade de animal (nessa ordem)
   analisaRecintos(tipoAnimal, quantidade) {
 
-    //4.2 - Caso animal informado seja inválido, apresentar erro "Animal inválido"
     if (!this.animais[tipoAnimal]) {
       return { erro: 'Animal inválido' };
     }
 
-    //5.2 - Caso quantidade informada seja inválida, apresentar erro "Quantidade inválida"
     if (quantidade <= 0 || isNaN(quantidade)) {
       return { erro: 'Quantidade inválida' };
     }
@@ -35,57 +32,41 @@ class RecintosZoo {
     const animalEnviadoInfo = this.animais[tipoAnimal];
     const recintosViaveis = [];
 
+    this.recintos.sort((a, b) => a.numero - b.numero);
+
     this.recintos.forEach(recintoAtual => {
-      console.log('-------------------------------');
-      console.log(`ANALISANDO O RECINTO ${recintoAtual.numero}...`);
-      console.log(`No recinto: ${recintoAtual.quantidade} ${recintoAtual.animaisExistentes}  | Enviado para o recinto: ${quantidade} ${tipoAnimal} `);
 
-  //3 - Animais já presentes no recinto devem continuar confortáveis com a inclusão do(s) novo(s)
-
-      //1 - Um animal se sente confortável se está num bioma adequado 
       if (!animalEnviadoInfo.biomas.some(bioma => recintoAtual.bioma.includes(bioma))) {
-        console.log(`Recinto não é adequado (bioma incompatível)`);
         return;
       }
 
-      //2 - Animais carnívoros devem habitar somente com a própria espécie
       const temOutrosAnimais = recintoAtual.animaisExistentes.length > 0;
       const temCarnivorosNoRecinto = recintoAtual.animaisExistentes.some(a => this.animais[a].carnivoro);
       const carnivorosDeOutraEspecie = recintoAtual.animaisExistentes.some(a => this.animais[a].carnivoro && a !== tipoAnimal);
-      console.log(`temOutrosAnimais ${temOutrosAnimais} | temCarnivorosNoRecinto ${temCarnivorosNoRecinto} | carnivorosDeOutraEspecie ${carnivorosDeOutraEspecie}`);
       
       if (animalEnviadoInfo.carnivoro) {
         if (temOutrosAnimais && carnivorosDeOutraEspecie) {
-          console.log(`Recinto não é viavel (carnívoros de espécies diferentes)`);
           return;
         } else if (temOutrosAnimais && !temCarnivorosNoRecinto && !carnivorosDeOutraEspecie) {
-          console.log(`Recinto não é viavel (carnívoros e um não-carnívoro)`);
           return;
         }
       } else {
         if (temCarnivorosNoRecinto) {
-          console.log(`Recinto não é viavel (não-carnívoro e um carnivoro)`);
           return;
         }
       }
 
-      //4 - Hipopótamo(s) só tolera(m) outras espécies estando num recinto com savana e rio
       if (tipoAnimal === 'HIPOPOTAMO' && !recintoAtual.animaisExistentes.includes('HIPOPOTAMO') && recintoAtual.animaisExistentes.length > 0 && !(recintoAtual.bioma.includes('savana') && recintoAtual.bioma.includes('rio'))) {
-        console.log(`Recinto não é adequado (Hipopótamos só toleram outras espécies em recintos com savana e rio)`);
         return;
       }
       if (recintoAtual.animaisExistentes.includes('HIPOPOTAMO') && tipoAnimal !== 'HIPOPOTAMO' && !(recintoAtual.bioma.includes('savana') && recintoAtual.bioma.includes('rio'))) {
-        console.log(`Recinto não é adequado (Hipopótamos só toleram outras espécies em recintos com savana e rio)`);
         return; 
       }
 
-      //5 - Um macaco não se sente confortável sem outro animal no recinto, seja da mesma ou outra espécie
       if (tipoAnimal == 'MACACO' && recintoAtual.animaisExistentes.length < 1 && quantidade <= 1) {
-        console.log(`Recinto não é adequado (macacos precisam de companhia)`);
         return;
       }
 
-      //6 - Quando há mais de uma espécie no mesmo recinto, é preciso considerar 1 espaço extra ocupado
       const espacoExtra = recintoAtual.animaisExistentes.some(animal => animal !== tipoAnimal && animal != null) ? 1 : 0;
 
       const espacoASerOcupado = (quantidade * animalEnviadoInfo.tamanho) + espacoExtra;
@@ -94,37 +75,25 @@ class RecintosZoo {
           const animal = recintoAtual.animaisExistentes[i];
           espacoJaOcupado += this.animais[animal].tamanho * recintoAtual.quantidade;
         }
-      
-      console.log(`Espaço total: ${recintoAtual.tamanhoTotal} | Espaço já ocupado: ${espacoJaOcupado}`);
-      console.log(`Espaço que será ocupado: ${espacoASerOcupado} (+ ${espacoExtra} espaço extra)`);
 
       const espacoDisponivel = (recintoAtual.tamanhoTotal - espacoJaOcupado);
       const espacoLivre = (recintoAtual.tamanhoTotal - espacoJaOcupado) - espacoASerOcupado;
-      console.log(`Espaço disponível: ${espacoDisponivel} | Espaço Livre: ${espacoLivre}`);
 
-      //7 - Não é possível separar os lotes de animais nem trocar os animais que já existem de recinto
       if (espacoDisponivel >= espacoASerOcupado) {
-        //3.2 - A lista de recintos viáveis deve indicar o espaço livre que restaria após a inclusão do(s) animal(is) e o espaço total, no formato "Recinto nro (espaço livre: valorlivre total: valortotal)"
         recintosViaveis.push(`Recinto ${recintoAtual.numero} (espaço livre: ${espacoLivre} total: ${recintoAtual.tamanhoTotal})`);
-        console.log(`Recinto é viável para o animal ${tipoAnimal}`);
-      } else {
-        console.log(`Recinto NÃO é viável para o animal ${tipoAnimal}`);
       }
     });
-    console.log('-------------------------------');
-    
-    //6.2 - Caso não haja recinto possível, apresentar erro "Não há recinto viável"
+
     if (recintosViaveis.length > 0) {
-      return { recintosViaveis };
+      console.log({recintosViaveis: recintosViaveis});
+      return { recintosViaveis: recintosViaveis };
     } else {
       return { erro: 'Não há recinto viável' };
     }
-    
+
   }
 }
 
-const zoo = new RecintosZoo();
-//2.2 - O programa deve retornar uma estrutura contendo a lista de todos os recintos viáveis ordenada pelo número do recinto (caso existam) e a mensagem de erro (caso exista)
-console.log(zoo.analisaRecintos('MACACO', 2)); 
+//new RecintosZoo().analisaRecintos('MACACO', 2);
 
 export { RecintosZoo as RecintosZoo };
